0149f044f7c09dd1e3dc9a86502a61b0dccb0624
Gaganrajput0912 <Gaganrajput717114@gmail.com>
2023-02-25 22:11:18 +0530

    first commit

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,25 +1,40 @@
-import { Fragment,useState } from "react";
+import { Fragment, useState } from "react";
+import { Route } from "react-router-dom";
 import Header from "./Components/Layout/Header/Header";
 import Body from "./Components/Layout/Body/body";
 import Footer from "./Components/Layout/Footer/Footer";
 import Cart from "./Components/Cart/Cart";
+import CartProvider from "./Components/Store/CartProvider";
+import AboutUs from "./Components/Pages/AboutUs";
+import Home from "./Components/Pages/Home";
 
 function App() {
-  const[cartIsShown,setCartIsShown]=useState(false)
+  const [cartIsShown, setCartIsShown] = useState(false);
 
-  const ShowCartHandler=()=>{
-    setCartIsShown(true)
-  }
+  const ShowCartHandler = () => {
+    setCartIsShown(true);
+  };
 
-  const HideCartHandler=()=>{
-    setCartIsShown(false)
-  }
+  const HideCartHandler = () => {
+    setCartIsShown(false);
+  };
   return (
     <Fragment>
-     { cartIsShown && <Cart onClose={HideCartHandler}/>}
-      <Header onShowCart={ShowCartHandler}/>
-      <Body/>
-      <Footer/>
+      <CartProvider>
+        {cartIsShown && <Cart onClose={HideCartHandler} />}
+        <Header onShowCart={ShowCartHandler} />
+        <Route path="/AboutUs">
+        <AboutUs/>
+      </Route>
+      <Route path="/Home">
+        <Home/>
+      </Route>
+        <Footer />
+      </CartProvider>
+
+
+      
+
     </Fragment>
   );
 }
